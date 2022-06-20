3074c9117ee58abbd1307c9eaaae8b56d77896f7
Hyeon Jin <choihj94@huray.net>
2022-06-20 01:00:50 +0000

    [ADD] add coco dataset moduel

---
+++ dataset/dataset.py
@@ -0,0 +1,14 @@
+
+from torch.utils.data import Dataset
+class COCODataset(Dataset):
+    
+    def __init__(self, annotation, data_dir, transform, train = True):
+        pass
+
+    def __getitem__(self, index: int) -> None:
+        pass
+        
+    def __len__(self) -> int:
+        return 0
+
+    
